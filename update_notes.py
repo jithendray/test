042f77854f45b0594e30ce9f44503f57b28bea0c
jithendray <jithendra.yenugula@gmail.com>
2024-10-07 00:35:49 +0530

    adding notes indecx

--- update_notes.py
+++ update_notes.py
@@ -69,8 +69,7 @@ title: Notes
 permalink: /notes
 ---
 
-A raw collection of my notes, seamlessly synced with my **OBSIDIAN** vault, capturing my ongoing explorations, 
-and personal learning across various domains, continuously updated as part of my knowledge base.
+A raw collection of my notes, seamlessly synced with my **OBSIDIAN** vault, capturing my ongoing explorations, and personal learning across various domains, continuously updated as part of my knowledge base.
 
 """
 
